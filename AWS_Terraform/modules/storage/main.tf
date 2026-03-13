185fc4926836b0baee2979a2214fa1f48980759f
prajwal <prajwalprajwal1999@gmail.com>
2026-03-13 17:05:01 +0530

    Minor format correction

--- AWS_Terraform/modules/storage/main.tf
+++ AWS_Terraform/modules/storage/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "ss-app-images" {
-  bucket = var.s3_bucket_name
+  bucket        = var.s3_bucket_name
   force_destroy = true
-  
+
   tags = {
     Project = "EKS_Project"
   }
